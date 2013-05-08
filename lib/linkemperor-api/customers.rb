91c12c620ba315489506ee08adb3e2d34056353c
Giorgenes <giorgenes@gmail.com>
2013-05-08 15:37:08 -0300

    improved formatting

--- lib/linkemperor-api/customers.rb
+++ lib/linkemperor-api/customers.rb
@@ -1,4 +1,3 @@
-
 require 'net/http'
 require 'uri'
 require 'json'
@@ -55,250 +54,163 @@ class LinkemperorCustomer
     end
   end
 
-  
+
   # This method returns a list of all the Articles that exist on your account.
   # Parameters:
   #  none
   def get_articles()
-    
-    
-    
-  
     exec_get("#{@base_path}/api/v2/customers/articles.json?api_key=#{@api_key}")
-  
   end
-  
+
   # This method returns details about the Article you specify.
   # Parameters:
   # - id: Article ID
   def get_articles_by_id(id)
-    
-    
-    
     if id.nil?
       raise LinkemperorApiException.new('id should not be empty')
     end
-    
-  
     exec_get("#{@base_path}/api/v2/customers/articles/#{id}.json?api_key=#{@api_key}")
-  
   end
-  
+
   # This method creates a new Article.
   # Parameters:
   # - campaign_id: Campaign ID for this Article
   # - title: Article Title (Spintax OK)
   # - body: Article Body (Spintax OK)
   def create_article(campaign_id, title, body)
-    
-    
-    
     if campaign_id.nil?
       raise LinkemperorApiException.new('campaign_id should not be empty')
     end
-    
+
     if title.nil?
       raise LinkemperorApiException.new('title should not be empty')
     end
-    
+
     if body.nil?
       raise LinkemperorApiException.new('body should not be empty')
     end
-    
-  
     parameters = {'article' => {'campaign_id' => campaign_id, 'title' => title, 'body' => body}}
     exec_post(parameters, 'post', "#{@base_path}/api/v2/customers/articles.json?api_key=#{@api_key}")
-  
   end
-  
+
   # This method deletes the Article you specify.
   # Parameters:
   # - id: Article ID
   def delete_article(id)
-    
-    
-    
     if id.nil?
       raise LinkemperorApiException.new('id should not be empty')
     end
-    
-  
     parameters = {}
     exec_post(parameters, 'delete', "#{@base_path}/api/v2/customers/articles/#{id}.json?api_key=#{@api_key}")
-  
   end
-  
+
   # This method returns a list of Articles for the Campaign.
   # Parameters:
   # - id: Campaign ID
   def get_campaign_articles(id)
-    
-    
-    
     if id.nil?
       raise LinkemperorApiException.new('id should not be empty')
     end
-    
-  
     exec_get("#{@base_path}/api/v2/customers/campaigns/#{id}/articles.json?api_key=#{@api_key}")
-  
   end
-  
+
   # This method returns a list of all the Blasts that exist on your account.
   # Parameters:
   #  none
   def get_blasts()
-    
-    
-    
-  
     exec_get("#{@base_path}/api/v2/customers/blasts.json?api_key=#{@api_key}")
-  
   end
-  
+
   # This method returns a details about the Blast you specify
   # Parameters:
   # - id: Blast ID
   def get_blast_by_id(id)
-    
-    
-    
     if id.nil?
       raise LinkemperorApiException.new('id should not be empty')
     end
-    
-  
     exec_get("#{@base_path}/api/v2/customers/blasts/#{id}.json?api_key=#{@api_key}")
-  
   end
-  
+
   # This method returns a list of the Blasts in the Campaign.
   # Parameters:
   # - id: Campaign ID
   def get_campaign_blasts(id)
-    
-    
-    
     if id.nil?
       raise LinkemperorApiException.new('id should not be empty')
     end
-    
-  
     exec_get("#{@base_path}/api/v2/customers/campaigns/#{id}/blasts.json?api_key=#{@api_key}")
-  
   end
-  
+
   # This method returns a list of all the Campagins that exist on your account.
   # Parameters:
   #  none
   def get_campaigns()
-    
-    
-    
-  
     exec_get("#{@base_path}/api/v2/customers/campaigns.json?api_key=#{@api_key}")
-  
   end
-  
+
   # This method returns details about the campaign you specify.
   # Parameters:
   # - id: Campaign ID
   def get_campaign_by_id(id)
-    
-    
-    
     if id.nil?
       raise LinkemperorApiException.new('id should not be empty')
     end
-    
-  
     exec_get("#{@base_path}/api/v2/customers/campaigns/#{id}.json?api_key=#{@api_key}")
-  
   end
-  
+
   # This method returns a list of the Sites in the Campaign.
   # Parameters:
   # - id: Campaign ID
   def get_campaign_sites(id)
-    
-    
-    
     if id.nil?
       raise LinkemperorApiException.new('id should not be empty')
     end
-    
-  
     exec_get("#{@base_path}/api/v2/customers/campaigns/#{id}/sites.json?api_key=#{@api_key}")
-  
   end
-  
+
   # This method returns a list of the Targets in the Campaign.
   # Parameters:
   # - id: Campaign ID
   def get_campaign_targets(id)
-    
-    
-    
     if id.nil?
       raise LinkemperorApiException.new('id should not be empty')
     end
-    
-  
     exec_get("#{@base_path}/api/v2/customers/campaigns/#{id}/targets.json?api_key=#{@api_key}")
-  
   end
-  
+
   # This method returns a list of Trouble Spots for the Campaign.
   # Parameters:
   # - id: Campaign ID
   def get_campaign_trouble_spots(id)
-    
-    
-    
     if id.nil?
       raise LinkemperorApiException.new('id should not be empty')
     end
-    
-  
     exec_get("#{@base_path}/api/v2/customers/campaigns/#{id}/trouble_spots.json?api_key=#{@api_key}")
-  
   end
-  
+
   # This method creates a new campaign.  Remember that if you exceed your plan limit on Campaigns, there may be additional charges.
   # Parameters:
   # - name: Name of the Campaign.
   # - notes: Notes
   def create_campaign(name, notes = nil)
-    
-    
-    
     if name.nil?
       raise LinkemperorApiException.new('name should not be empty')
     end
-    
-  
     parameters = {'campaign' => {'name' => name, 'notes' => notes}}
     exec_post(parameters, 'post', "#{@base_path}/api/v2/customers/campaigns.json?api_key=#{@api_key}")
-  
   end
-  
+
   # This method deletes the Campaign you specify.
   # Parameters:
   # - id: Campaign ID
   def delete_campaign(id)
-    
-    
-    
     if id.nil?
       raise LinkemperorApiException.new('id should not be empty')
     end
-    
-  
     parameters = {}
     exec_post(parameters, 'delete', "#{@base_path}/api/v2/customers/campaigns/#{id}.json?api_key=#{@api_key}")
-  
   end
-  
+
   # This method is used to purchase link building.<br /><br />
   # We call a single purchase an Order, and each order can contain multiple Blasts.<br /><br />
   # First, you'll need to determine which of our link building Services you'd like to order.  Use the /services endpoint of the API to get a list of available services.<br /><br />
@@ -310,36 +222,24 @@ class LinkemperorCustomer
   # - custom: You may provide any string here. We will save it as part of the Order and include it in the returned data whenever you check on an Order's status. It's great for holding your internal ID number for the Order.
   # - requests: This is where the actual object describing your order goes.  This is either a JSON nested array or XML nested tags (depending on your current format).  The schema for this field is described below in the section titled Schema Used In Your Request.
   def create_order(requests, how_pay = nil, callback_url = nil, custom = nil)
-    
-    
-    
     if requests.nil?
       raise LinkemperorApiException.new('requests should not be empty')
     end
-    
-  
     parameters = {'order' => {'how_pay' => how_pay, 'callback_url' => callback_url, 'custom' => custom, 'requests' => requests}}
     exec_post(parameters, 'post', "#{@base_path}/api/v2/customers/orders.json?api_key=#{@api_key}")
-  
   end
-  
+
   # This method shows the details of an Order and its component Blasts.<be /><be />
   # It's a great way to check on an order or obtain a list of Built URLs to report back to your systems.
   # Parameters:
   # - id: ID # of the Order
   def get_order_by_id(id)
-    
-    
-    
     if id.nil?
       raise LinkemperorApiException.new('id should not be empty')
     end
-    
-  
     exec_get("#{@base_path}/api/v2/customers/orders/#{id}.json?api_key=#{@api_key}")
-  
   end
-  
+
   # If you're going to order link building, you need to check which Services are currently available.<br /><br />
   # This list will change on a day-to-day or even minute-to-minute basis,
   # so please look up the Services list to determine the best Services to order before placing an Order.<br /><br />
@@ -347,55 +247,34 @@ class LinkemperorCustomer
   # Parameters:
   #  none
   def get_services()
-    
-    
-    
-  
     exec_get("#{@base_path}/api/v2/customers/services.json?api_key=#{@api_key}")
-  
   end
-  
+
   # This method returns a list of the currently available Services that
   # cdon't build links on Adult or other potentially objectional sites.
   # Parameters:
   #  none
   def get_safe_services()
-    
-    
-    
-  
     exec_get("#{@base_path}/api/v2/customers/services/safe.json?api_key=#{@api_key}")
-  
   end
-  
+
   # This method returns a list of all the Sites that exist on your account.
   # Parameters:
   #  none
   def get_sites()
-    
-    
-    
-  
     exec_get("#{@base_path}/api/v2/customers/sites.json?api_key=#{@api_key}")
-  
   end
-  
+
   # This method returns details about the Site you specify.
   # Parameters:
   # - id: Site ID
   def get_site_by_id(id)
-    
-    
-    
     if id.nil?
       raise LinkemperorApiException.new('id should not be empty')
     end
-    
-  
     exec_get("#{@base_path}/api/v2/customers/sites/#{id}.json?api_key=#{@api_key}")
-  
   end
-  
+
   # This method creates a new Site.
   # Parameters:
   # - campaign_id: Campaign ID for this Site
@@ -403,209 +282,139 @@ class LinkemperorCustomer
   # - domain_name: Domain Name of this Site
   # - rss_feed: RSS Feed for this Site
   def create_site(campaign_id, name, domain_name, rss_feed = nil)
-    
-    
-    
     if campaign_id.nil?
       raise LinkemperorApiException.new('campaign_id should not be empty')
     end
-    
+
     if name.nil?
       raise LinkemperorApiException.new('name should not be empty')
     end
-    
+
     if domain_name.nil?
       raise LinkemperorApiException.new('domain_name should not be empty')
     end
-    
-  
     parameters = {'site' => {'campaign_id' => campaign_id, 'name' => name, 'domain_name' => domain_name, 'rss_feed' => rss_feed}}
     exec_post(parameters, 'post', "#{@base_path}/api/v2/customers/sites.json?api_key=#{@api_key}")
-  
   end
-  
+
   # This method deletes the Site you specify.
   # Parameters:
   # - id: Site ID
   def delete_site(id)
-    
-    
-    
     if id.nil?
       raise LinkemperorApiException.new('id should not be empty')
     end
-    
-  
     parameters = {}
     exec_post(parameters, 'delete', "#{@base_path}/api/v2/customers/sites/#{id}.json?api_key=#{@api_key}")
-  
   end
-  
+
   # This method returns a list of all the Targets that exist on your account (across all Campaigns).
   # Parameters:
   #  none
   def get_targets()
-    
-    
-    
-  
     exec_get("#{@base_path}/api/v2/customers/targets.json?api_key=#{@api_key}")
-  
   end
-  
+
   # This method returns details about the Target you specify.
   # Parameters:
   # - id: Target ID
   def get_target_by_id(id)
-    
-    
-    
     if id.nil?
       raise LinkemperorApiException.new('id should not be empty')
     end
-    
-  
     exec_get("#{@base_path}/api/v2/customers/targets/#{id}.json?api_key=#{@api_key}")
-  
   end
-  
+
   # This method creates a new Target.  You will need to provide a Campaign ID and a URL for the target.
   # Parameters:
   # - campaign_id: Campaign ID
   # - url_input: Fully qualified URL for the target.
   # - keyword_input: Keywords to add to the target.  Separate multiple keywords with linebreaks.
   def create_target(campaign_id, url_input, keyword_input = nil)
-    
-    
-    
     if campaign_id.nil?
       raise LinkemperorApiException.new('campaign_id should not be empty')
     end
-    
+
     if url_input.nil?
       raise LinkemperorApiException.new('url_input should not be empty')
     end
-    
-  
     parameters = {'target' => {'campaign_id' => campaign_id, 'url_input' => url_input, 'keyword_input' => keyword_input}}
     exec_post(parameters, 'post', "#{@base_path}/api/v2/customers/targets.json?api_key=#{@api_key}")
-  
   end
-  
+
   # This method deletes the Target you specify.
   # Parameters:
   # - id: Target ID
   def delete_target(id)
-    
-    
-    
     if id.nil?
       raise LinkemperorApiException.new('id should not be empty')
     end
-    
-  
     parameters = {}
     exec_post(parameters, 'delete', "#{@base_path}/api/v2/customers/targets/#{id}.json?api_key=#{@api_key}")
-  
   end
-  
+
   # This method returns a list of all the Keywords that exist on your account.  You can optionally limit the list to those keywords that belong to a specific campaign or target.
   # Parameters:
   # - target_id: Limit keywords to those belonging to this target.
   # - campaign_id: Limit keywords to those belonging to this campaign.
   def get_target_keywords(target_id = nil, campaign_id = nil)
-    
-    
-    
-  
     exec_get("#{@base_path}/api/v2/customers/target_keywords.json?api_key=#{@api_key}")
-  
   end
-  
+
   # This method returns details about the Keyword you specify.
   # Parameters:
   # - id: Keyword ID
   def get_target_keyword_by_id(id)
-    
-    
-    
     if id.nil?
       raise LinkemperorApiException.new('id should not be empty')
     end
-    
-  
     exec_get("#{@base_path}/api/v2/customers/target_keywords/#{id}.json?api_key=#{@api_key}")
-  
   end
-  
+
   # This method creates a new Keyword.  You will need to provide a Target ID.
   # Parameters:
   # - target_id: Target ID
   # - keyword_string: Keyword string
   def create_target_keyword(target_id, keyword_string)
-    
-    
-    
     if target_id.nil?
       raise LinkemperorApiException.new('target_id should not be empty')
     end
-    
+
     if keyword_string.nil?
       raise LinkemperorApiException.new('keyword_string should not be empty')
     end
-    
-  
     parameters = {'target_keyword' => {'target_id' => target_id, 'keyword_string' => keyword_string}}
     exec_post(parameters, 'post', "#{@base_path}/api/v2/customers/target_keywords.json?api_key=#{@api_key}")
-  
   end
-  
+
   # This method deletes the Keyword you specify.
   # Parameters:
   # - id: Keyword ID
   def delete_target_keyword(id)
-    
-    
-    
     if id.nil?
       raise LinkemperorApiException.new('id should not be empty')
     end
-    
-  
     parameters = {}
     exec_post(parameters, 'delete', "#{@base_path}/api/v2/customers/target_keywords/#{id}.json?api_key=#{@api_key}")
-  
   end
-  
+
   # This method returns a list of all the Trouble Spots that exist on your account.
   # 
   # Trouble Spots are issues spotted by our On-Page SEO Checker for Campaigns.
   # Parameters:
   #  none
   def get_trouble_spots()
-    
-    
-    
-  
     exec_get("#{@base_path}/api/v2/customers/trouble_spots.json?api_key=#{@api_key}")
-  
   end
-  
+
   # This method returns details about the Trouble Spot you specify.
   # Parameters:
   # - id: TroubleSpot ID
   def get_trouble_spot_by_id(id)
-    
-    
-    
     if id.nil?
       raise LinkemperorApiException.new('id should not be empty')
     end
-    
-  
     exec_get("#{@base_path}/api/v2/customers/trouble_spots/#{id}.json?api_key=#{@api_key}")
-  
   end
-  
+
 end
-          
--- lib/linkemperor-api/vendors.rb
+++ lib/linkemperor-api/vendors.rb
@@ -1,4 +1,3 @@
-
 require 'net/http'
 require 'uri'
 require 'json'
@@ -55,7 +54,7 @@ class LinkemperorVendor
     end
   end
 
-  
+
   # We call orders placed for your link building Service a Blast.
   # Use this method to retrieve all outstanding orders for your link building service(s).
   # 
@@ -63,57 +62,36 @@ class LinkemperorVendor
   # Parameters:
   # - service_id: ID of a Service.  If provided, the response will be scoped to just that service ID.
   def get_blasts(service_id = nil)
-    
-    
-    
-  
     exec_get("#{@base_path}/api/v2/vendors/blasts.json?api_key=#{@api_key}")
-  
   end
-  
+
   # Pulls the next blast from the order queue, marks it as having been started,
   # and returns full information about the Blast, including Targets and Output URLs, if available.
   # Parameters:
   # - service_id: ID of a Service.  If provided, the response will be scoped to just that service ID.
   def get_next_blast(service_id = nil)
-    
-    
-    
-  
     exec_get("#{@base_path}/api/v2/vendors/blasts/next.json?api_key=#{@api_key}")
-  
   end
-  
+
   # Pulls a batch of blast from the order queue, marks them as having been started,
   # and returns full information about the Blast, including Targets and Output URLs, if available.
   # Parameters:
   # - service_id: ID of a Service.  If provided, the response will be scoped to just that service ID.
   # - batch_size: Batch size.  If not provided, the default batch size is 100
   def get_next_batch_blasts(service_id = nil, batch_size = nil)
-    
-    
-    
-  
     exec_get("#{@base_path}/api/v2/vendors/blasts/next_batch.json?api_key=#{@api_key}")
-  
   end
-  
+
   # Returns the full details of a Blast.  Make sure to provide a Blast ID.
   # Parameters:
   # - id: ID # of the Blast
   def get_blast_by_id(id)
-    
-    
-    
     if id.nil?
       raise LinkemperorApiException.new('id should not be empty')
     end
-    
-  
     exec_get("#{@base_path}/api/v2/vendors/blasts/#{id}.json?api_key=#{@api_key}")
-  
   end
-  
+
   # Once you've completed link building for a request, you need to submit the URLs where links were built.  This PUT method does that.
   # 
   # After we receive this submission, we will verify the links provided within 24 hours.
@@ -123,87 +101,57 @@ class LinkemperorVendor
   # - id: ID # of the Blast
   # - links: A string containing the list of links to submit (newline delimited)
   def submit_built_link(id, links)
-    
-    
-    
     if id.nil?
       raise LinkemperorApiException.new('id should not be empty')
     end
-    
+
     if links.nil?
       raise LinkemperorApiException.new('links should not be empty')
     end
-    
-  
     parameters = {'blast' => {'links' => links}}
     exec_post(parameters, 'put', "#{@base_path}/api/v2/vendors/blasts/#{id}.json?api_key=#{@api_key}")
-  
   end
-  
+
   # Lists all available Services.  This is a great way to automatically compare your service against the current competition.
   # Parameters:
   #  none
   def get_services()
-    
-    
-    
-  
     exec_get("#{@base_path}/api/v2/vendors/services.json?api_key=#{@api_key}")
-  
   end
-  
+
   # Lists the full details of a specific Service.
   # Parameters:
   # - id: ID # of the Service
   def get_service_by_id(id)
-    
-    
-    
     if id.nil?
       raise LinkemperorApiException.new('id should not be empty')
     end
-    
-  
     exec_get("#{@base_path}/api/v2/vendors/services/#{id}.json?api_key=#{@api_key}")
-  
   end
-  
+
   # This API method looks at all the Built URLs submitted to a given Service in the last 7 days and finds domains that have never passed our link checker.
   # 
   # This is a great way to clean your list of URLs used for submissions.
   # Parameters:
   # - id: ID # of the Service
   def get_failed_domains(id)
-    
-    
-    
     if id.nil?
       raise LinkemperorApiException.new('id should not be empty')
     end
-    
-  
     exec_get("#{@base_path}/api/v2/vendors/services/#{id}/failed_domains.json?api_key=#{@api_key}")
-  
   end
-  
+
   # Creates a test blast for your Service.  It will not affect your score or marketplace rank.  However, if you submit URLs that fail to pass our link checker, they will be reflected in the failed_domains method of the API.
   # 
   # This is particularly useful for testing new URL lists or potential link sources.
   # Parameters:
   # - id: ID # of the Service
   def create_test_blast(id)
-    
-    
-    
     if id.nil?
       raise LinkemperorApiException.new('id should not be empty')
     end
-    
-  
     parameters = {}
     exec_post(parameters, 'post', "#{@base_path}/api/v2/vendors/services/#{id}/test_blast.json?api_key=#{@api_key}")
-  
   end
-  
+
 end
-          
